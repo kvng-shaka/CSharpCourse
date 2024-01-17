4a6b544bff97cf20d0e8f6a90077fc4dd750e38a
Funsho Adeosun <funsho.adeosun01@gmail.com>
2024-01-17 15:05:51 +0100

    getting familiar with c-sharp

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World! This is my first C# Program!");
+
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Car Model: ");
+        string? carModel = Console.ReadLine();
+
+        Console.Write("Enter Plate Number: ");
+        string? 
+        plateNumber = Console.ReadLine();
+
+        Console.Write("Enter Number of Hours Parked: ");
+        int hoursParked = Convert.ToInt32(Console.ReadLine());
+
+        double fee = ParkingFee(hoursParked);
+
+        Console.WriteLine("<================================================>");
+        Console.WriteLine($"Car Model: {carModel}");
+        Console.WriteLine($"Plate Number: {plateNumber}");
+        Console.WriteLine($"Hours Parked: {hoursParked}");
+        Console.WriteLine($"Total Parking Fee: NGN {fee}");
+    }
+
+
+    public static double ParkingFee(int hoursParked)
+    {
+        const double firstHour = 500.00;
+        const double subsequentHour = 100.00;
+
+        int extraHours = hoursParked - 1;
+        double fee = firstHour + (subsequentHour * extraHours);
+
+        return fee;
+    }
+}
+
